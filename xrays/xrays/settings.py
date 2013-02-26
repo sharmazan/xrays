b2ad404971895d05e2d3ab5b19306622d683c482
Sergey Sharmazanov <sergey.sharm@gmail.com>
2013-02-26 19:35:19 +0200

    Add app Catalog

--- xrays/xrays/settings.py
+++ xrays/xrays/settings.py
@@ -15,11 +15,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'xrays',                      # Or path to database file if using sqlite3.
+        'USER': 'xrays',                      # Not used with sqlite3.
+        'PASSWORD': 'xrays',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -125,6 +125,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'xrays.xrays.apps.catalog',
+
 )
 
 # A sample logging configuration. The only tangible logging
