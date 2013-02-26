20c87e08506168e1946e1962c85fb7b23cd542a2
Sergey Sharmazanov <sergey.sharm@gmail.com>
2013-02-26 21:55:25 +0200

    Added patients app

--- xrays/xrays/settings.py
+++ xrays/xrays/settings.py
@@ -15,12 +15,19 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'xrays',                      # Or path to database file if using sqlite3.
-        'USER': 'xrays',                      # Not used with sqlite3.
-        'PASSWORD': 'xrays',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'xray.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'xrays',                      # Or path to database file if using sqlite3.
+#        'USER': 'xrays',                      # Not used with sqlite3.
+#        'PASSWORD': 'xrays',                  # Not used with sqlite3.
+#        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -122,11 +129,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    'xrays.xrays.apps.catalog',
+    # 'xrays.apps.catalog',
+    #'catalog',
+    'xrays.apps.catalog',
 
 )
 
